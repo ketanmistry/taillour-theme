9a760261bc1d3ae6ed77a0cf05aa554ee4febff9
Ketan Mistry <6877442+ketanmistry@users.noreply.github.com>
2025-10-15 11:20:02 +1000

    Applied number stepper to cart line items.

--- assets/cart.js
+++ assets/cart.js
@@ -7,40 +7,40 @@ function updateLineItemQty(el) {
   let id = el.dataset.id;
   let quantity = el.value;
   let row = el.closest('.js--cart-item');
-  if (quantity != "" && quantity >= 0) {
+  if (quantity != '' && quantity >= 0) {
     let data = {
       id: key,
-      quantity: quantity
+      quantity: quantity,
     };
     fetch('/cart/change.js', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data)
-      })
-      .then(response => response.json())
-      .then(cart => {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((cart) => {
         console.log(cart);
         if (quantity > 0) {
-          row.querySelectorAll('.js--cart-item-total').forEach(function(el) {
+          row.querySelectorAll('.js--cart-item-total').forEach(function (el) {
             // Get details of the updated item.
-            let item = cart.items.filter(function(lineitem) {
+            let item = cart.items.filter(function (lineitem) {
               if (lineitem.key === key) {
                 return lineitem;
               }
             });
-            el.innerText = Shopify.formatMoney((item[0].final_line_price));
+            el.innerText = Shopify.formatMoney(item[0].final_line_price);
           });
         } else {
           row.remove();
           removeGiftWrap(key);
         }
         // Update the total cart price.
-        document.querySelectorAll('.js--cart-total').forEach(function(el) {
+        document.querySelectorAll('.js--cart-total').forEach(function (el) {
           el.innerText = Shopify.formatMoney(cart.total_price);
         });
-        if (typeof window.spendMore === "function") {
+        if (typeof window.spendMore === 'function') {
           spendMore(cart);
         }
         // Update cart item count.
@@ -51,7 +51,14 @@ function updateLineItemQty(el) {
         }
       });
   }
-};
+}
+
+/*
+ * Cart: Update line item on stepper click.
+ */
+function qtyStepperHandler(el) {
+  setTimeout(updateLineItemQty, 300, el);
+}
 
 /*
  * GIFT WRAP OPTION
@@ -59,7 +66,7 @@ function updateLineItemQty(el) {
  */
 function runGiftWrap() {
   var $this = event.target;
-  var lineItemTitle = $this.getAttribute("data-item-title");
+  var lineItemTitle = $this.getAttribute('data-item-title');
   var lineItemKey = $this.value;
   if ($this.checked == true) {
     addGiftWrap(lineItemTitle, lineItemKey);
@@ -76,29 +83,31 @@ function runGiftWrap() {
  */
 function addGiftWrap(lineItemTitle, lineItemKey) {
   var data = {
-    items: [{
-      id: giftWrap.id,
-      quantity: 1,
-      properties: {
-        Item: lineItemTitle,
-        _id: lineItemKey,
+    items: [
+      {
+        id: giftWrap.id,
+        quantity: 1,
+        properties: {
+          Item: lineItemTitle,
+          _id: lineItemKey,
+        },
       },
-    }, ],
+    ],
   };
 
-  fetch("/cart/add.js", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
+  fetch('/cart/add.js', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
     .then((response) => response.json())
     .then((update) => {
-      fetch("/cart.json")
+      fetch('/cart.json')
         .then((response) => response.json())
         .then((cart) => {
-          document.querySelectorAll(".js--cart-total").forEach(function(el) {
+          document.querySelectorAll('.js--cart-total').forEach(function (el) {
             el.textContent = Shopify.formatMoney(cart.total_price);
           });
           updateCartCounter(cart);
@@ -113,26 +122,26 @@ function addGiftWrap(lineItemTitle, lineItemKey) {
  * @param lineItemKey (string) - Cart line item key
  */
 function removeGiftWrap(lineItemKey) {
-  fetch("/cart.json")
+  fetch('/cart.json')
     .then((response) => response.json())
     .then((cart) => {
-      var item = cart.items.filter(function(item) {
+      var item = cart.items.filter(function (item) {
         if (item.properties) {
-          if (item.properties["_id"] == lineItemKey) {
+          if (item.properties['_id'] == lineItemKey) {
             data = {
               id: item.key,
               quantity: 0,
             };
-            fetch("/cart/change.js", {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-              })
+            fetch('/cart/change.js', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(data),
+            })
               .then((response) => response.json())
               .then((cart) => {
-                document.querySelectorAll(".js--cart-total").forEach(function(el) {
+                document.querySelectorAll('.js--cart-total').forEach(function (el) {
                   el.textContent = Shopify.formatMoney(cart.total_price);
                 });
                 updateCartCounter(cart);
@@ -148,7 +157,7 @@ function removeGiftWrap(lineItemKey) {
  * Update cart item count.
  */
 function updateCartCounter(cart) {
-  document.querySelectorAll('.js--cart-counter').forEach(function(el) {
+  document.querySelectorAll('.js--cart-counter').forEach(function (el) {
     el.innerText = cart.item_count;
   });
 }
