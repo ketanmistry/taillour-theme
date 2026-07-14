142798ab354fbd8ec2aa3db1a6daa1109ac9b065
Ketan Mistry <6877442+ketanmistry@users.noreply.github.com>
2026-07-14 16:42:20 +1000

    Removed gift wrap.

--- assets/cart.js
+++ assets/cart.js
@@ -6,24 +6,24 @@ function updateLineItemQty(el) {
   let key = el.dataset.key;
   let id = el.dataset.id;
   let quantity = el.value;
-  let row = el.closest('.js--cart-item');
-  if (quantity != '' && quantity >= 0) {
+  let row = el.closest('[data-js="cart-item"]');
+  if (quantity != "" && quantity >= 0) {
     let data = {
       id: key,
       quantity: quantity,
     };
-    fetch('/cart/change.js', {
-      method: 'POST',
+    fetch("/cart/change.js", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     })
       .then((response) => response.json())
       .then((cart) => {
-        console.log(cart);
+        // console.log(cart); // @debug
         if (quantity > 0) {
-          row.querySelectorAll('.js--cart-item-total').forEach(function (el) {
+          row.querySelectorAll('[data-js="item-total"]').forEach(function (el) {
             // Get details of the updated item.
             let item = cart.items.filter(function (lineitem) {
               if (lineitem.key === key) {
@@ -34,20 +34,19 @@ function updateLineItemQty(el) {
           });
         } else {
           row.remove();
-          removeGiftWrap(key);
         }
         // Update the total cart price.
-        document.querySelectorAll('.js--cart-total').forEach(function (el) {
+        document.querySelectorAll('[data-js="cart-total"]').forEach(function (el) {
           el.innerText = Shopify.formatMoney(cart.total_price);
         });
-        if (typeof window.spendMore === 'function') {
+        if (typeof window.spendMore === "function") {
           spendMore(cart);
         }
         // Update cart item count.
-        updateCartCounter(cart);
+        _updateCartItemCount(cart.item_count);
         // Display empty cart if no items.
         if (cart.item_count == 0) {
-          document.querySelector('#cart-content').innerHTML = emptyCartHtml;
+          document.querySelector("#cart-content").innerHTML = emptyCartHtml;
         }
       });
   }
@@ -58,106 +57,4 @@ function updateLineItemQty(el) {
  */
 function qtyStepperHandler(el) {
   setTimeout(updateLineItemQty, 300, el);
-}
-
-/*
- * GIFT WRAP OPTION
- * Manages the gift wrap option in the cart.
- */
-function runGiftWrap() {
-  var $this = event.target;
-  var lineItemTitle = $this.getAttribute('data-item-title');
-  var lineItemKey = $this.value;
-  if ($this.checked == true) {
-    addGiftWrap(lineItemTitle, lineItemKey);
-  } else {
-    removeGiftWrap(lineItemKey);
-  }
-}
-
-/*
- * ADD GIFT WRAP ITEM
- * Adds gift wrap product to cart.
- * @param lineItemTitle - Product title of the cart line item
- * @param lineItemKey (string) - Cart line item key
- */
-function addGiftWrap(lineItemTitle, lineItemKey) {
-  var data = {
-    items: [
-      {
-        id: giftWrap.id,
-        quantity: 1,
-        properties: {
-          Item: lineItemTitle,
-          _id: lineItemKey,
-        },
-      },
-    ],
-  };
-
-  fetch('/cart/add.js', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((update) => {
-      fetch('/cart.json')
-        .then((response) => response.json())
-        .then((cart) => {
-          document.querySelectorAll('.js--cart-total').forEach(function (el) {
-            el.textContent = Shopify.formatMoney(cart.total_price);
-          });
-          updateCartCounter(cart);
-          spendMore(cart);
-        });
-    });
-}
-
-/*
- * REMOVE GIFT WRAP ITEM
- * Removes gift wrap item from the cart by setting its quantity to 0.
- * @param lineItemKey (string) - Cart line item key
- */
-function removeGiftWrap(lineItemKey) {
-  fetch('/cart.json')
-    .then((response) => response.json())
-    .then((cart) => {
-      var item = cart.items.filter(function (item) {
-        if (item.properties) {
-          if (item.properties['_id'] == lineItemKey) {
-            data = {
-              id: item.key,
-              quantity: 0,
-            };
-            fetch('/cart/change.js', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(data),
-            })
-              .then((response) => response.json())
-              .then((cart) => {
-                document.querySelectorAll('.js--cart-total').forEach(function (el) {
-                  el.textContent = Shopify.formatMoney(cart.total_price);
-                });
-                updateCartCounter(cart);
-                spendMore(cart);
-              });
-          }
-        }
-      });
-    });
-}
-
-/*
- * Update cart item count.
- */
-function updateCartCounter(cart) {
-  document.querySelectorAll('.js--cart-counter').forEach(function (el) {
-    el.innerText = cart.item_count;
-  });
 }
