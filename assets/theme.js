2a964ee9c1daa02b811d56ed9f87e239dfb6f9b7
Ketan Mistry <6877442+ketanmistry@users.noreply.github.com>
2026-07-17 10:58:08 +1000

    Cart Drawer, plus other changes (#41)
    * Update settings_schema.json
    
    * Working cart drawer with ajax add to cart and update line item functions.
    
    * Update theme.js
    
    * Improve purchase-together cart flow
    
    Add loading spinner and disable button during cart submission. Use Shopify.routes.root for proper routing, update cart drawer dynamically instead of redirecting, and refresh cart item count after items are added.
    
    * Close cart drawer on escape keypress.
    
    * Added "waiting" icon in cart drawer.
    
    * Update theme.css
    
    * Removed gift wrap.
    
    * Replaced blocks with liquid blocks.
    
    * Replaced section headling/description settings with blocks.
    
    * Updated image-with-text section to include video and code options.
    
    * Renamed section to "media-with-text".
    
    * Removed the "custom liquid with text" section.
    
    This is now handled by the media-with-text section.
    
    * Applied blocks to promotions section.
    
    * Applied blocks to products section.
    
    * Applied blocks to collections section.
    
    * Added "content_card" block for multicolumns section.
    
    * Added "collection_card" block.
    
    * Converted product cards to blocks.
    
    * Converted articles card to blocks.
    
    * Converted contact form to block templates.
    
    * Update main-list-collections.liquid
    
    Applied collection card block.
    
    * Applied product card block to collection template.
    
    * Applied collection card block to collection template.
    
    * Fixed issue with recently viewed section.
    
    * Update product-recommendations.liquid
    
    * Update page.contact.json
    
    * Applied product card block to search template.
    
    * Applied buttons block to buttons section.
    
    * Converted the text section to blocks.
    
    * Update buttons.liquid
    
    * Update index.json
    
    * Converted FAQs section into blocks.
    
    * Categorised sections.
    
    * Converted the product details blocks into block templates.
    
    * Moved the product form into the block.
    
    * Update _product_detail_cart.liquid
    
    * Update cart-drawer.liquid
    
    * Display discount code field on cart page.
    
    * Added target and nofollow attributes to button block.
    
    * Replaced range with numbers.
    
    * Reverted the sub-collections.
    
    * Update main-collection-sub.liquid
    
    * Update main-collection-sub.liquid
    
    * Basic working featured products section.
    
    * Make SEO robots metatag configurable
    
    Convert hardcoded noindex logic into a configurable snippet with theme settings. Adds support for matching pages by exact path or path prefix, and includes settings for managing canonical URLs. The noindex functionality is now controlled through the theme customizer rather than being hardcoded.
    
    * Added canonical tag theme settings.
    
    * Added logo theme setting for structured data use.
    
    * Update main-collection-products.liquid

--- assets/theme.js
+++ assets/theme.js
@@ -1,8 +1,8 @@
 // Define a convenience method and use it like
 // ready(() => {...});
 var ready = (callback) => {
-  if (document.readyState != 'loading') callback();
-  else document.addEventListener('DOMContentLoaded', callback);
+  if (document.readyState != "loading") callback();
+  else document.addEventListener("DOMContentLoaded", callback);
 };
 
 /*
@@ -18,12 +18,12 @@ function historyState() {
  * Checks for url parameter called `name`.
  */
 function getParam(name) {
-  if ('URLSearchParams' in window) {
+  if ("URLSearchParams" in window) {
     var params = new URLSearchParams(window.location.search);
     return params.get(name);
   } else {
     // Polyfill for IE11
-    var params = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    var params = new RegExp("[\?&]" + name + "=([^&#]*)").exec(window.location.href);
     if (params == null) {
       return null;
     } else {
@@ -37,6 +37,7 @@ ready(function () {
   handleVariantSelector();
   handleCloseDropdownMenus();
   handleNavToggles();
+  handleAjaxAddToCart();
 });
 
 /*
@@ -45,23 +46,23 @@ ready(function () {
  * Referenced from https://gist.github.com/stewartknapman/8d8733ea58d2314c373e94114472d44c
  */
 var Shopify = Shopify || {};
-Shopify.money_format = '${{amount}}';
+Shopify.money_format = "${{amount}}";
 Shopify.formatMoney = function (cents, format) {
-  if (typeof cents == 'string') {
-    cents = cents.replace('.', '');
+  if (typeof cents == "string") {
+    cents = cents.replace(".", "");
   }
-  var value = '';
+  var value = "";
   var placeholderRegex = /\{\{\s*(\w+)\s*\}\}/;
   var formatString = format || this.money_format;
 
   function defaultOption(opt, def) {
-    return typeof opt == 'undefined' ? def : opt;
+    return typeof opt == "undefined" ? def : opt;
   }
 
   function formatWithDelimiters(number, precision, thousands, decimal) {
     precision = defaultOption(precision, 2);
-    thousands = defaultOption(thousands, ',');
-    decimal = defaultOption(decimal, '.');
+    thousands = defaultOption(thousands, ",");
+    decimal = defaultOption(decimal, ".");
 
     if (isNaN(number) || number == null) {
       return 0;
@@ -69,25 +70,25 @@ Shopify.formatMoney = function (cents, format) {
 
     number = (number / 100.0).toFixed(precision);
 
-    var parts = number.split('.'),
-      dollars = parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1' + thousands),
-      cents = parts[1] ? decimal + parts[1] : '';
+    var parts = number.split("."),
+      dollars = parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1" + thousands),
+      cents = parts[1] ? decimal + parts[1] : "";
 
     return dollars + cents;
   }
 
   switch (formatString.match(placeholderRegex)[1]) {
-    case 'amount':
+    case "amount":
       value = formatWithDelimiters(cents, 2);
       break;
-    case 'amount_no_decimals':
+    case "amount_no_decimals":
       value = formatWithDelimiters(cents, 0);
       break;
-    case 'amount_with_comma_separator':
-      value = formatWithDelimiters(cents, 2, '.', ',');
+    case "amount_with_comma_separator":
+      value = formatWithDelimiters(cents, 2, ".", ",");
       break;
-    case 'amount_no_decimals_with_comma_separator':
-      value = formatWithDelimiters(cents, 0, '.', ',');
+    case "amount_no_decimals_with_comma_separator":
+      value = formatWithDelimiters(cents, 0, ".", ",");
       break;
   }
 
@@ -106,17 +107,17 @@ function handleVariantSelector() {
   options[2] = null;
   options[3] = null;
 
-  var variantOptions = document.querySelectorAll('.js--variant-option');
+  var variantOptions = document.querySelectorAll(".js--variant-option");
   variantOptions.forEach(function (el) {
-    el.addEventListener('change', function (event) {
+    el.addEventListener("change", function (event) {
       // Disable non-existent variant options.
       checkVariants();
 
       // Set the chosen options.
       variantOptions.forEach(function (opt) {
-        if (opt.tagName.toLowerCase() == 'input' && opt.checked == true) {
-          var optionName = opt.getAttribute('name');
-          var optionPos = parseInt(optionName.replace('option', ''));
+        if (opt.tagName.toLowerCase() == "input" && opt.checked == true) {
+          var optionName = opt.getAttribute("name");
+          var optionPos = parseInt(optionName.replace("option", ""));
           var optionValue = opt.value;
           options[optionPos] = optionValue;
         }
@@ -128,11 +129,11 @@ function handleVariantSelector() {
           variant = v;
 
           // Update variant id and prices
-          document.querySelector('input#js--variant-id').value = v.id;
+          document.querySelector("input#js--variant-id").value = v.id;
           // The following is used for the "purchase together" feature.
           document.querySelectorAll('input[type="checkbox"].js--variant-id').forEach(function (el) {
             el.value = v.id;
-            el.setAttribute('data-price', v.price);
+            el.setAttribute("data-price", v.price);
             if (v.available == true) {
               el.checked = true;
               el.disabled = false;
@@ -141,46 +142,46 @@ function handleVariantSelector() {
               el.disabled = true;
             }
           });
-          document.querySelectorAll('.js--variant-price').forEach(function (el) {
+          document.querySelectorAll(".js--variant-price").forEach(function (el) {
             el.innerHTML = Shopify.formatMoney(v.price);
           });
 
           if (v.compare_at_price > v.price) {
-            document.querySelectorAll('.js--variant-compareatprice').forEach(function (el) {
+            document.querySelectorAll(".js--variant-compareatprice").forEach(function (el) {
               el.innerText = Shopify.formatMoney(v.compare_at_price);
             });
           } else {
-            document.querySelectorAll('.js--variant-compareatprice').forEach(function (el) {
-              el.innerText = '';
+            document.querySelectorAll(".js--variant-compareatprice").forEach(function (el) {
+              el.innerText = "";
             });
           }
 
           // Update SKU
-          document.querySelectorAll('.js--variant-sku').forEach(function (el) {
+          document.querySelectorAll(".js--variant-sku").forEach(function (el) {
             el.innerText = variant.sku;
           });
 
           // Disable the buy button if product is unavailable
           if (v.available === false) {
-            document.querySelector('#js--addtocart').disabled = true;
-            document.querySelector('#js--addtocart').innerText = 'Unavailable';
+            document.querySelector('[data-js="addtocart"]').disabled = true;
+            document.querySelector('[data-js="addtocart"]').innerText = "Unavailable";
           } else {
-            document.querySelector('#js--addtocart').disabled = false;
-            document.querySelector('#js--addtocart').innerText = 'Add to cart';
+            document.querySelector('[data-js="addtocart"]').disabled = false;
+            document.querySelector('[data-js="addtocart"]').innerText = "Add to cart";
           }
 
           // Update image
           var imageId = v.featured_image != null ? v.featured_image.id : default_image;
           var imageIndex = document
             .querySelector('.product-image-item[data-imageid="' + imageId + '"]')
-            .getAttribute('data-index');
-          var sliderContainer = document.querySelector('.product-images-component');
+            .getAttribute("data-index");
+          var sliderContainer = document.querySelector(".product-images-component");
           goToImage(imageIndex, sliderContainer);
 
           // Append the variant ID as a url parameter
           if (v != undefined) {
             if (historyState()) {
-              window.history.replaceState({}, document.title, '?variant=' + v.id);
+              window.history.replaceState({}, document.title, "?variant=" + v.id);
             }
           }
         }
@@ -224,7 +225,7 @@ function checkVariants() {
 
     // Then, loop through each input and check if its value is in the optionGroups
     // array created above, ignoring the clicked option group.
-    document.querySelectorAll('.js--variant-option').forEach(function (input) {
+    document.querySelectorAll(".js--variant-option").forEach(function (input) {
       if (input.name != $this.name) {
         if (optionGroups[input.name].includes(input.value) == false) {
           input.disabled = true;
@@ -237,11 +238,11 @@ function checkVariants() {
   }
 
   // Check a valid option is selected for each group.
-  document.querySelectorAll('.js--variant-options').forEach(function (group) {
+  document.querySelectorAll(".js--variant-options").forEach(function (group) {
     let firstAvailable = null;
-    let checkedOptions = group.querySelectorAll('.js--variant-option:checked').length;
+    let checkedOptions = group.querySelectorAll(".js--variant-option:checked").length;
     if (checkedOptions == 0) {
-      firstAvailable = group.querySelectorAll('.js--variant-option:not(:disabled)')[0];
+      firstAvailable = group.querySelectorAll(".js--variant-option:not(:disabled)")[0];
     }
     if (firstAvailable != null) {
       firstAvailable.checked = true;
@@ -255,8 +256,8 @@ function checkVariants() {
  * and the rest is done in CSS.
  */
 function toggleNav() {
-  let nav = document.querySelector('#header-nav');
-  nav.classList.toggle('nav-open');
+  let nav = document.querySelector("#header-nav");
+  nav.classList.toggle("nav-open");
 }
 
 /*
@@ -266,23 +267,23 @@ function toggleNav() {
 function handleNumberStepper() {
   let maxQty = 20;
   let minQty = 1;
-  const steppers = document.querySelectorAll('.input-stepper');
+  const steppers = document.querySelectorAll(".input-stepper");
 
   steppers.forEach(function (stepper) {
     let input = stepper.querySelector('input[type="number"]');
-    let stepperButtons = stepper.querySelectorAll('button');
-    let maxValue = input.getAttribute('max') !== undefined ? parseInt(input.getAttribute('max')) : maxQty;
-    let minValue = input.getAttribute('min') !== undefined ? parseInt(input.getAttribute('min')) : minQty;
+    let stepperButtons = stepper.querySelectorAll("button");
+    let maxValue = input.getAttribute("max") !== undefined ? parseInt(input.getAttribute("max")) : maxQty;
+    let minValue = input.getAttribute("min") !== undefined ? parseInt(input.getAttribute("min")) : minQty;
 
     stepperButtons.forEach(function (button) {
-      button.addEventListener('click', function (event) {
+      button.addEventListener("click", function (event) {
         let currentValue = parseInt(input.value);
-        if (button.classList.contains('input-stepper_decrease')) {
+        if (button.classList.contains("input-stepper_decrease")) {
           if (currentValue > minValue) {
             input.value = currentValue - 1;
           }
         }
-        if (button.classList.contains('input-stepper_increase')) {
+        if (button.classList.contains("input-stepper_increase")) {
           if (currentValue < maxValue) {
             input.value = currentValue + 1;
           }
@@ -298,11 +299,11 @@ function handleNumberStepper() {
  * for keyboard users/accessibility.
  */
 function handleCloseDropdownMenus() {
-  const nav = document.querySelector('#header-nav');
-  document.addEventListener('keydown', function (event) {
-    if (event.key === 'Escape') {
-      nav.querySelectorAll('details[open]').forEach((detail) => {
-        detail.removeAttribute('open');
+  const nav = document.querySelector("#header-nav");
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      nav.querySelectorAll("details[open]").forEach((detail) => {
+        detail.removeAttribute("open");
       });
     }
   });
@@ -313,10 +314,10 @@ function handleCloseDropdownMenus() {
  * Handles the nav toggles, so only one menu is open at a  time.
  */
 function handleNavToggles() {
-  const nav = document.getElementById('header-nav');
-  let details = nav.querySelectorAll('details');
+  const nav = document.getElementById("header-nav");
+  let details = nav.querySelectorAll("details");
   details.forEach(function (detail) {
-    detail.addEventListener('toggle', function (event) {
+    detail.addEventListener("toggle", function (event) {
       if (detail.open) {
         details.forEach((otherDetail) => {
           if (otherDetail !== detail && otherDetail.open) {
@@ -325,5 +326,115 @@ function handleNavToggles() {
         });
       }
     });
+  });
+}
+
+/*
+ * ADD ITEM TO CART.
+ */
+function handleAjaxAddToCart() {
+  const btns = document.querySelectorAll('[data-js="addtocart"]');
+  btns.forEach((btn) => {
+    let parentForm = btn.closest("form");
+    btn.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      // Replace button text with waiting icon and disable it.
+      let btnLabel = btn.innerHTML.trim();
+      btn.innerHTML = `
+        <svg style="margin:0 auto;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style="shape-rendering: auto; display: block; background: none;" width="16" height="16" xmlns:xlink="http://www.w3.org/1999/xlink"><g><circle stroke-dasharray="188.49555921538757 64.83185307179586" r="40" stroke-width="15" stroke="#ffffff" fill="none" cy="50" cx="50">
+          <animateTransform keyTimes="0;1" values="0 50 50;360 50 50" dur="1s" repeatCount="indefinite" type="rotate" attributeName="transform"></animateTransform>
+        </circle><g></g></g></svg>`;
+      btn.disabled = true;
+
+      // Get the variant details and add to cart.
+      let variantId = parentForm.querySelector('input[name="id"]').value;
+      let quantity = parentForm.querySelector('input[name="quantity"]').value;
+      let productData = {
+        items: [
+          {
+            id: variantId,
+            quantity: parseInt(quantity),
+          },
+        ],
+        sections: ["cart-drawer"],
+      };
+      fetch(window.Shopify.routes.root + "cart/add.js", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(productData),
+      })
+        .then((response) => response.json())
+        .then((cart) => {
+          if (document.getElementById("shopify-section-cart-drawer")) {
+            document.getElementById("shopify-section-cart-drawer").outerHTML = cart.sections["cart-drawer"];
+          }
+          cartDrawer.showModal();
+          btn.innerHTML = btnLabel;
+          btn.disabled = false;
+          fetch(window.Shopify.routes.root + "cart.js")
+            .then((response) => response.json())
+            .then((cart) => {
+              _updateCartItemCount(cart.item_count);
+            });
+        });
+    });
+  });
+}
+
+/*
+ * UPDATE CART LINE ITEM
+ * Updates/removes the line item from cart drawer.
+ */
+function updateCartLineItem(el) {
+  let key = el.dataset.key;
+  let qty = el.value;
+  if (qty !== "" && qty >= 0) {
+    let cartItemsSection = document.querySelector("#cartDrawer-items");
+    document.getElementById("cartDrawer-content").classList.add("waiting");
+
+    let data = {
+      updates: { [key]: qty },
+      sections: ["cart-drawer"],
+    };
+    fetch(window.Shopify.routes.root + "cart/update.js", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((cart) => {
+        if (document.getElementById("shopify-section-cart-drawer")) {
+          document.getElementById("shopify-section-cart-drawer").outerHTML = cart.sections["cart-drawer"];
+        }
+        _updateCartItemCount(cart.item_count);
+        document.getElementById("cartDrawer-content").classList.remove("waiting");
+        cartDrawer.showModal();
+      });
+  }
+}
+
+/*
+ * UPDATE CART ITEM COUNT
+ */
+function _updateCartItemCount(count) {
+  let instances = document.querySelectorAll('[data-js="cart-counter"]');
+  instances.forEach((el) => {
+    el.innerText = count;
+  });
+}
+
+/*
+ * CLOSE CART DRAWER ON ESCAPE KEYPRESS
+ */
+function handleCloseCartDrawer() {
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      cartDrawer.close();
+    }
   });
 }
